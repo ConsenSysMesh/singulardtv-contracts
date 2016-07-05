b679674fcec7921acfafc0fbaaf54e79f81f61c5
Milad Mostavi <milad.mostavi@gmail.com>
2016-07-05 16:18:19 +0300

    Removed standard tokens

--- token_contracts/SampleRecipientThrow.sol
+++
@@ -1,9 +0,0 @@
-/*
-This is an example contract that helps test the functionality of the approveAndCall() functionality of HumanStandardToken.sol.
-This one will throw and thus needs to propagate the error up.
-*/
-contract SampleRecipientThrow {
-  function () {
-    throw;
-  }
-}
