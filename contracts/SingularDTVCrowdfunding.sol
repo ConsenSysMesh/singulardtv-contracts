4a93cc243d50657950782eea8042f4445d0bc7b3
Milad Mostavi <milad.mostavi@gmail.com>
2016-08-28 17:57:40 +0300

    Changed CROWDFUNDING_PERIOD, TOKEN_LOCKING_PERIOD and TOKEN_TARGET for public test

--- contracts/SingularDTVCrowdfunding.sol
+++ contracts/SingularDTVCrowdfunding.sol
@@ -16,9 +16,9 @@ contract SingularDTVCrowdfunding {
      *  Constants
      */
     uint constant public CAP = 1000000000; // 1B tokens is the maximum amount of tokens
-    uint constant public CROWDFUNDING_PERIOD = 4 weeks; // 1 month
-    uint constant public TOKEN_LOCKING_PERIOD = 2 years; // 2 years
-    uint constant public TOKEN_TARGET = 534000000; // 34M Tokens more than the initial 500M, around 42,500 ETH
+    uint constant public CROWDFUNDING_PERIOD = 5 days; // 1 month
+    uint constant public TOKEN_LOCKING_PERIOD = 2 days; // 2 years
+    uint constant public TOKEN_TARGET = 502000000; // 34M Tokens more than the initial 500M, around 42,500 ETH
 
     /*
      *  Enums
